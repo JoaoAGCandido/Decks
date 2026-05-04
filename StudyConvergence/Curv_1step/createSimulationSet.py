54cb00a45c8c7c11aeb61ebbcfa73eee82469172
Joao Candido <joao.a.g.candido@tecnico.ulisboa.pt>
2026-05-04 18:05:59 +0100

    run more different steps

--- StudyConvergence/Curv_1step/createSimulationSet.py
+++ StudyConvergence/Curv_1step/createSimulationSet.py
@@ -17,7 +17,7 @@ pushers = [
     # SimOpts('gcaCorrV4', ["1000", "500", "100", "50", "10", "1", "0_1"], 'gca_corr'),
     # SimOpts('gcaCorrV5', ["1000", "500", "100", "50", "10", "1", "0_1"], 'gca_corr'),
     # SimOpts('gcaCorrNoBoris', ["1000", "500", "100", "50", "10", "1", "0_1"], 'gca_corr'),
-    SimOpts('gcaNoDrifts_doublePrecDiag_and_Gca', ["1000", "500", "100", "50", "10", "1", "0_1"], 'gca')
+    SimOpts('gcaNoDrifts_doublePrecDiag_and_Gca', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca')
 ]
 
 BASE_DTW = 0.01
