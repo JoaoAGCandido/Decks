cbf49d86fa29f8731e4552fbaaee47ab9f50db89
Joao Candido <joao.a.g.candido@tecnico.ulisboa.pt>
2026-05-25 14:39:07 +0100

    test mirror force

--- StudyConvergence/gcaRefactor/createSimulationSet.py
+++ StudyConvergence/gcaRefactor/createSimulationSet.py
@@ -15,6 +15,7 @@ class SimOpts:
         branch=None,
         worktree_folder=None,
         n_cells=DEFAULT_N_CELLS,
+        test="curv",
     ):
         self.name = name
         self.dtw_values = dtw_values
@@ -24,6 +25,7 @@ class SimOpts:
         self.branch = branch if branch is not None else name
         self.worktree_folder = worktree_folder if worktree_folder is not None else self.branch
         self.n_cells = n_cells
+        self.test = test
 
 
 pushers = [
@@ -36,7 +38,8 @@ pushers = [
     # SimOpts('gcaNoDrifts_doublePrecDiag_and_Gca', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca')
     # SimOpts('GcaHighRes', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca')
     # SimOpts('gcaNoDrifts_doublePrecDiag_and_Gca_nx180', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', branch="gcaNoDrifts_doublePrecDiag_and_Gca", worktree_folder='gcaNoDrifts_doublePrecDiag_and_Gca'),
-    SimOpts('Gcav4', ["100", "1", "0_01"], 'gca', branch="deucalion_gca", n_cells=80),
+    # SimOpts('Gcav4', ["100", "1", "0_01"], 'gca', branch="deucalion_gca", n_cells=80),
+    SimOpts('Gcav4_Mirror', ["100", "1", "0_01"], 'gca', branch="deucalion_gca", n_cells=80, test="mirror"),
 
 ]
 
@@ -46,7 +49,7 @@ NDUMP_TRACKS_ONE_FROM_DTW = 10.0 # means that for dtw >= 10, we dump tracks ever
 TRACKS_MODE = "per_dtw_1000"  # "per_dtw_1000" or "legacy"
 RAW_SOURCE_RELATIVE_PATH = Path("MS/RAW/test_electrons/RAW-test_electrons-000000.h5")
 
-INPUT_TEMPLATE = """simulation
+CURV_TEMPLATE = """simulation
 {{
   enforce_rng_constancy  = .true.,
   disableEmfAdvance = .True.,
@@ -181,6 +184,146 @@ type(1:3) = "5pass", "5pass", "5pass",
 }}
 """
 
+MIRROR_TEMPLATE = """simulation
+{{
+  enforce_rng_constancy  = .true.,
+  disableEmfAdvance = .True.,
+}}
+
+node_conf
+{{
+  node_number(1:3) =  2, 2, 2,
+  if_periodic(1:3) =  .true., .true., .true.,
+}}
+
+!----------spatial grid----------
+grid
+{{
+  nx_p(1:3) =  {n_cells}, {n_cells}, {n_cells},
+  coordinates = "cartesian",
+}}
+
+!----------time step and global data dump timestep number----------
+time_step
+{{
+  dt     =   {dt},
+  ndump  =    1,
+}}
+
+!----------restart information----------
+restart
+{{
+  ndump_fac = 0,
+  if_restart = .false.,
+}}
+
+!----------spatial limits of the simulations----------
+space
+{{
+  xmin(1:3) =  0.000d0, 0.000d0, 0.000d0,
+  xmax(1:3) =  40., 40., 40.,
+  if_move(1:3) = .false., .false., .false.,
+}}
+
+!----------time limits ----------
+time
+{{
+  tmin = 0.0d0, tmax  = 125.66370614359172953850573533118,
+}}
+
+!----------field solver set up----------
+el_mag_fld
+{{
+  ext_fld = "static",
+  type_init_b(1:3) = "math func", "math func", "math func",
+  init_b_mfunc(1) = "-0.1*x1*x3",
+  init_b_mfunc(2) = "-0.1*x2*x3",
+  init_b_mfunc(3) = "1000.0 + 0.1*x3*x3",
+
+  type_init_e(1:3) = "math func", "math func", "math func",
+  init_e_mfunc(1) = "0.0",
+  init_e_mfunc(2) = "0.0",
+  init_e_mfunc(3) = "0.0",
+}}
+
+!----------boundary conditions for em-fields ----------
+emf_bound
+{{
+  type(1:2,1) =   "open", "open",
+  type(1:2,2) =   "open", "open",
+  type(1:2,3) =   "open", "open",
+}}
+
+!----------diagnostic for electromagnetic fields----------
+diag_emf
+{{
+
+}}
+
+!----------number of particle species----------
+particles
+{{
+  interpolation = "cubic",
+  num_species = 1,
+}}
+
+!----------information for test species----------
+species
+{{
+  name = "test_electrons",
+  num_par_max = 6000000,
+  rqm = -1.0,
+  num_par_x(1:3) = 2,2,2,
+  add_tag = .true.,
+  push_type = {pusher},
+}}
+
+!----------inital proper velocities----------
+udist {{
+  ufl(1:3)=  0.5d0 , 0.5d0 , 0.1d0 ,
+  uth(1:3)=  0.01d0 , -0.01d0 , 0.01d0 ,
+}}
+
+!----------density profile for this species----------
+profile
+{{
+  profile_type(1) = "math func",
+  math_func_expr = "if((x1<23)&&(x1>17)&&(x2<23)&&(x2>17)&&(x3<22.5)&&(x3>21.5), 1.0, 0.0)",
+  den_min = 1.d-12,
+}}
+
+!----------boundary conditions for this species----------
+spe_bound
+{{
+type(1:2,1) =    "open",    "open",
+type(1:2,2) =    "open",    "open",
+type(1:2,3) =    "open",    "open",
+}}
+
+!----------diagnostic for this species----------
+diag_species
+{{
+  ndump_fac_ene = 1,
+  ndump_fac_raw = 1000000,
+  {tag_comment}ndump_fac_tracks = {ndump_fac_tracks},
+  {tag_comment}niter_tracks = {niter_tracks},
+  {tag_comment}file_tags = "tag_file_osiris_utils.tag",
+  {tag_comment}ifdmp_tracks_efl(1:3) = .true., .true., .true.,
+  {tag_comment}ifdmp_tracks_bfl(1:3) = .true., .true., .true.,
+}}
+
+!-------------smooth for currents-------------
+smooth
+{{
+type(1:3) = "5pass", "5pass", "5pass",
+}}
+"""
+
+TEST_TEMPLATES = {
+    "curv": CURV_TEMPLATE,
+    "mirror": MIRROR_TEMPLATE,
+}
+
 RUNJOB_WORKTREE_TEMPLATE = """#!/bin/bash
 #SBATCH --job-name={job_name}
 #SBATCH --account=f202500196cpcaa3a
@@ -285,11 +428,20 @@ def job_name_from(base_dir, sim_opts, dtw):
     return f"{test_name}{sim_opts.name}{dtw_label}"
 
 
-def write_input_files(base_dir, pushers, tags=False, template=INPUT_TEMPLATE):
+def template_for_test(test):
+    try:
+        return TEST_TEMPLATES[test]
+    except KeyError:
+        options = ", ".join(sorted(TEST_TEMPLATES))
+        raise ValueError(f"Unknown test template: {test}. Expected one of: {options}") from None
+
+
+def write_input_files(base_dir, pushers, tags=False, template=None):
     base_path = Path(base_dir)
     created_files = []
 
     for sim_opts in pushers:
+        input_template = template if template is not None else template_for_test(sim_opts.test)
         for dtw in sim_opts.dtw_values:
             dtw_label = dtw_to_label(dtw)
             dtw_path = base_path / sim_opts.name / f"dtw{dtw_label}"
@@ -298,7 +450,7 @@ def write_input_files(base_dir, pushers, tags=False, template=INPUT_TEMPLATE):
             input_file = dtw_path / f"{sim_opts.name}.in"
             dt_value = dt_from_label(dtw)
             input_file.write_text(
-                template.format(
+                input_template.format(
                     n_cells=sim_opts.n_cells,
                     dt=f"{dt_value:.30f}",
                     tmax=f"{dt_value:.30f}",
@@ -395,7 +547,7 @@ def write_tag_files(base_dir, pushers, tag_source_path=None, output_name="tag_fi
 
 if __name__ == "__main__":
     root = Path("/home/exxxx5/Tese/Decks/StudyConvergence/gcaRefactor")
-    TAGS = True
+    TAGS = False
     TAG_SOURCE_PATH = "/home/exxxx5/Tese/Decks/StudyConvergence/Curv_1step/GcaStable_nx80/dtw0_1/tag_file_osiris_utils.tag"
 
     created_dirs = create_simulation_tree(root, pushers)
