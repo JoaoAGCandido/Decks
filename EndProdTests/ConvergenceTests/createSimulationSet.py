f9ffe6e27d1cfac599b676613444418f00cb19cc
Joao Candido <joao.a.g.candido@tecnico.ulisboa.pt>
2026-06-30 15:08:21 +0100

    gca_exp

--- EndProdTests/ConvergenceTests/createSimulationSet.py
+++ EndProdTests/ConvergenceTests/createSimulationSet.py
@@ -41,14 +41,14 @@ pushers = [
     # SimOpts('GcaHighRes', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca')
     # SimOpts('gcaNoDrifts_doublePrecDiag_and_Gca_nx180', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', branch="gcaNoDrifts_doublePrecDiag_and_Gca", worktree_folder='gcaNoDrifts_doublePrecDiag_and_Gca'),
     # SimOpts('Gcav4', ["100", "1", "0_01"], 'gca', branch="deucalion_gca", n_cells=80),
-    SimOpts('Curv_Gca_nx80', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=80, test="curv"),
-    SimOpts('Curv_Gca_nx120', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=120, test="curv"),
-    SimOpts('Curv_Gca_nx160', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=160, test="curv"),
-    SimOpts('Curv_Gca_nx200', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=200, test="curv"),
-    SimOpts('Mirror_Gca_nx80', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=80, test="mirror"),
-    SimOpts('Mirror_Gca_nx120', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=120, test="mirror"),
-    SimOpts('Mirror_Gca_nx160', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=160, test="mirror"),
-    SimOpts('Mirror_Gca_nx200', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca', False, n_cells=200, test="mirror"),
+    SimOpts('Curv_Gca_expl_nx80', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=80, test="curv"),
+    SimOpts('Curv_Gca_expl_nx120', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=120, test="curv"),
+    SimOpts('Curv_Gca_expl_nx160', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=160, test="curv"),
+    SimOpts('Curv_Gca_expl_nx200', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=200, test="curv"),
+    SimOpts('Mirror_Gca_expl_nx80', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=80, test="mirror"),
+    SimOpts('Mirror_Gca_expl_nx120', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=120, test="mirror"),
+    SimOpts('Mirror_Gca_expl_nx160', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=160, test="mirror"),
+    SimOpts('Mirror_Gca_expl_nx200', ["1000", "500", "100", "50", "10", "1", "0_1", "0_01", "0_001", "0_0001"], 'gca_expl', False, n_cells=200, test="mirror"),
 ]
 
 BASE_DTW = 0.01
@@ -150,11 +150,11 @@ species
   add_tag = .true.,
   push_type = {pusher},
   !active_gca_components = "all", !"EXB", "gradB", "curv", "vEgradb", "E_par_F", "vEbgradb_F", "mirror_F",
-  gca_pos_max_iter = 100,
-  gca_mirror_max_iter = 100,
-  gca_pos_res = 1.0e-15,
-  gca_mirror_tol = 1.0e-15,
-  gca_force_explicit_mirror = .false.,
+  !gca_pos_max_iter = 100,
+  !gca_mirror_max_iter = 100,
+  !gca_pos_res = 1.0e-15,
+  !gca_mirror_tol = 1.0e-15,
+  !gca_force_explicit_mirror = .false.,
   !push_start_time = {push_start_time},
 }}
 
@@ -574,7 +574,7 @@ def write_tag_files(base_dir, pushers, tag_source_path=None, output_name="tag_fi
 
 if __name__ == "__main__":
     root = Path("EndProdTests/ConvergenceTests")
-    TAGS = True
+    TAGS = False
     # TAG_SOURCE_PATH = "/home/exxxx5/Tese/Decks/StudyConvergence/gcaRefactor/Gcav4/dtw0_01/tag_file_osiris_utils.tag"
     # TAG_SOURCE_PATH = "/home/exxxx5/Tese/Decks/StudyConvergence/gcaRefactor/Gcav4_Mirror/dtw0_01/tag_file_osiris_utils.tag"
     TAG_SOURCE_PATH = None
